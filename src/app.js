36b1e00b19af98c0a2c8ca98e1f37a380a2931e0
sai <nukarapusai@gmail.com>
2026-03-12 23:03:16 +0530

    fix jest server running issue

--- src/app.js
+++ src/app.js
@@ -12,8 +12,11 @@ app.use("/users", usersRoute)
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// Start server only if not in test environment
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
 module.exports = app
